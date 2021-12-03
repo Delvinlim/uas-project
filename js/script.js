ef77af295349c6316e2a1f48342d94f9c6c348d3
Delvin Lim <tkjpublic@gmail.com>
2021-12-03 22:03:02 +0700

    First Commit adding existing repository

---
+++ js/script.js
@@ -0,0 +1,19 @@
+AOS.init();
+
+var textWrapper = document.querySelector('.home .content h3');
+textWrapper.innerHTML = textWrapper.textContent.replace(/\S/g, "<span class='letter'>$&</span>");
+
+anime.timeline({loop: false})
+  .add({
+    targets: '.home .content h3 .letter',
+    opacity: [0,1],
+    easing: "easeInOutQuad",
+    duration: 1250,
+    delay: (el, i) => 80 * (i+1)
+  }).add({
+    targets: '.home .content h3',
+    opacity: 1,
+    duration: 520,
+    easing: "easeOutExpo",
+    delay: 1000
+  });
