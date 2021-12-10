52d79dddc7a2fce1169735f3e735bb14fef931eb
Delvin Lim <tkjpublic@gmail.com>
2021-12-11 02:10:22 +0700

    lazy loading img, server config success

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 var express = require('express')
 var app = express()
 var fs = require('fs')
-const { Http2ServerRequest } = require('http2')
 var path = require('path')
 var http = require('http').Server(app)
 // var port = 3000
@@ -19,14 +18,10 @@ app.get('/suit', (req, res) =>{
   res.sendFile(__dirname + '/suit-game/index.html')
 })
 
-// var ipaddress = process.env.OPENSHIFT_NODEJS_IP || "127.0.0.1";
 
+// Server Configuration
 var port = process.env.PORT || 8080;
 
 http.listen(port, () => {
   console.log(`Express server listening on port ${port}`);
 })
-
-// app.listen(port, () => {
-//   console.log(`testing using express listening at http://localhost:${port}`);
-// })
